724d647b12f4e1a7b1495992b29d9e479e3ec145
Stoke Zhang <zaqthss2009@gmail.com>
2017-10-08 16:48:01 +0800

    codes and readme

---
+++ src/cn/edu/thu/screen/util/Assist.java
@@ -0,0 +1,86 @@
+package cn.edu.thu.screen.util;
+
+import cn.edu.thu.screen.entity.TimePoint;
+import cn.edu.thu.screen.entity.TimeSeries;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by Stoke on 2017/10/8.
+ * E-mail address is zaqthss2009@gmail.com
+ * Copyright © Stoke. All Rights Reserved.
+ *
+ * @author Stoke
+ */
+public class Assist {
+  public static String PATH = "data/";
+
+  /**
+   * Basic attributes: timestamp, dirty, label, truth
+   *
+   * @param filename filename
+   * @param index which column besides timestamp should be read
+   * @param splitOp to split up the lines
+   * @return data in time series form
+   */
+  public TimeSeries readData(String filename, int index, String splitOp) {
+    TimeSeries timeSeries = new TimeSeries();
+
+    try {
+      FileReader fr = new FileReader(PATH + filename);
+      BufferedReader br = new BufferedReader(fr);
+
+      String line;
+      long timestamp;
+      double value;
+      TimePoint tp;
+
+      while ((line = br.readLine()) != null) {
+        String[] vals = line.split(splitOp);
+        timestamp = Long.parseLong(vals[0]);
+        value = Double.parseDouble(vals[index]);
+
+        tp = new TimePoint(timestamp, value);
+        timeSeries.addTimePoint(tp);
+      }
+
+      br.close();
+      fr.close();
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return timeSeries;
+  }
+
+  /**
+   * RMS sqrt(|modify - truth|^2 / len)
+   *
+   * @param truthSeries
+   * @param resultSeries
+   * @return
+   */
+  public double calcRMS(TimeSeries truthSeries, TimeSeries resultSeries) {
+    double cost = 0;
+    double delta;
+    int len = truthSeries.getLength();
+    int labelNum = 0;
+
+    for (int i = 0; i < len; ++i) {
+      delta = resultSeries.getTimeseries().get(i).getModify()
+          - truthSeries.getTimeseries().get(i).getValue();
+
+      cost += delta * delta;
+    }
+    cost /= (len - labelNum);
+
+    return Math.sqrt(cost);
+  }
+
+}
